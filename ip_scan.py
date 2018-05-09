1b040351de21ffd608b9c04b79d6422d5c2ad85a
Chen, Junchao <junchao.chen@analog.com>
2018-05-09 15:52:26 +0800

    Support User defined gateway address user type 192.168.1.1, will scan 192.168.1.2-251

--- ip_scan.py
+++ ip_scan.py
@@ -10,16 +10,21 @@ import sys,socket,threading
 
 
 
-def run_thread(start,stop):
+def run_thread(prefix,start,stop):
 	for i in xrange(start,stop):
 		try:
-			r = socket.gethostbyaddr('10.99.23.{}'.format(i))
+			r = socket.gethostbyaddr(prefix+'.{}'.format(i))
 			if '2.7.' in sys.version:    print r[0],r[-1]
 			else:                        print(r[0],r[-1])
 		except:
 			continue
 	
 t_s=[]
+
+if '2.7.' in sys.version:    baseaddr = raw_input('Input gateway addr\n')
+else:                        baseaddr = input('Input gateway addr\n')
+prefix = '.'.join( baseaddr.split('.')[:-1])
+
 for i in range(10):
-	t_s.append( threading.Thread(target=run_thread, args=(25*i+2,25*i+25+2)) )
+	t_s.append( threading.Thread(target=run_thread, args=(prefix,25*i+2,25*i+25+2)) )
 	t_s[-1].start()
