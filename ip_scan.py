ae46fb4bb7dc7c3f46319a63076f324ee165e80c
Chen, Junchao <junchao.chen@analog.com>
2018-05-10 11:16:25 +0800

    Add Function: Print available address in case some device hasn't hostname

--- ip_scan.py
+++ ip_scan.py
@@ -8,14 +8,16 @@ Last edit:   20180509    ignore_warning max_cnt for screen capture
 """
 import sys,socket,threading
 
-
+not_s = []
 
 def run_thread(prefix,start,stop):
+	global not_s
 	for i in xrange(start,stop):
 		try:
 			r = socket.gethostbyaddr(prefix+'.{}'.format(i))
 			if '2.7.' in sys.version:    print r[0],r[-1]
 			else:                        print(r[0],r[-1])
+			not_s.append(i)
 		except:
 			continue
 	
@@ -28,3 +30,16 @@ prefix = '.'.join( baseaddr.split('.')[:-1])
 for i in range(10):
 	t_s.append( threading.Thread(target=run_thread, args=(prefix,25*i+2,25*i+25+2)) )
 	t_s[-1].start()
+
+for i in t_s:    i.join()
+	
+	
+all_ip_s = range(2,252)
+for i in not_s:
+	if i in all_ip_s:    all_ip_s.remove(i)
+
+if '2.7.' in sys.version:    print 'Available IP:'
+else:                        print('Available IP:')
+for i in all_ip_s:
+	if '2.7.' in sys.version:    print i
+	else:                        print(i)
